0d3a1abdc87aef046f9a188a09d77196f87e8edd
Chao Zhang <zhang.chao.eric@gmail.com>
2013-08-01 14:37:25 -0400

    Modified the package name from \'stack\' to \'stackandqueue\'. Uhh, the reason is obvious, you know it...

---
+++ src/main/java/cse/buffalo/edu/algorithms/stackandqueue/FixedCapacityStackTest.java
@@ -0,0 +1,21 @@
+package cse.buffalo.edu.algorithms.stackandqueue;
+
+import cse.buffalo.edu.algorithms.stdlib.*;
+
+public class FixedCapacityStackTest {
+  public static void main(String[] args) {
+      FixedCapacityStack<String> s;
+      s = new FixedCapacityStack<String>(100);
+
+      while(!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            if (!item.equals("-")) {
+                    s.push(item);
+                  } else if (!s.isEmpty()) {
+                          StdOut.print(s.pop() +  " ");
+                        }
+          }
+
+      StdOut.println("(" + s.size() + " left on stack)");
+    }
+}
---
+++ src/main/java/cse/buffalo/edu/algorithms/stackandqueue/Stack.java
@@ -0,0 +1,81 @@
+package cse.buffalo.edu.algorithms.stackandqueue;
+
+import cse.buffalo.edu.algorithms.stdlib.StdIn;
+import cse.buffalo.edu.algorithms.stdlib.StdOut;
+import java.util.Iterator;
+
+public class Stack<Item> implements Iterable<Item> {
+
+  private Node first;
+  private int N;
+
+  private class Node {
+    Item item;
+    Node next;
+  }
+
+  public Stack() {
+    first = null;
+    N = 0;
+  }
+
+  public boolean isEmpty() {
+    return first == null;
+  }
+
+  public int size() {
+    return N;
+  }
+
+  public void push(Item item) {
+    // Add item to top of stack
+    Node orifirst = first;
+    first = new Node();
+    first.item = item;
+    first.next = orifirst;
+    N++;
+  }
+
+  public Item pop() {
+    // Remove item from top of stack
+    Item item = first.item;
+    first = first.next;
+    N--;
+    return item;
+  }
+
+  public Iterator<Item> iterator() {
+    return new LinkedListStackIterator();
+  }
+
+  private class LinkedListStackIterator implements Iterator<Item> {
+    Node head = first;
+
+    public boolean hasNext() {
+      return head != null;
+    }
+
+    public Item next() {
+      Item item = head.item;
+      head = head.next;
+      return item;
+    }
+
+    public void remove() {
+      // Pass
+    }
+  }
+
+  public static void main(String[] args) {
+    Stack<String> s = new Stack<String>();
+    while(!StdIn.isEmpty()) {
+      String item = StdIn.readString();
+      if (!item.equals("-")) {
+        s.push(item);
+      } else if (!s.isEmpty()) {
+        StdOut.print(s.pop() + " ");
+      }
+    }
+    StdOut.println("(" + s.size() + " left on stack)");
+  }
+}
