e5cefe28a1e6c8f92174b67a5125df6a658d68e4
Chao Zhang <zhang.chao.eric@gmail.com>
2013-08-04 23:39:31 -0400

    Implemented insertion sort.

---
+++ src/main/java/cse/buffalo/edu/algorithms/sort/Insertion.java
@@ -0,0 +1,36 @@
+package cse.buffalo.edu.algorithms.sort;
+
+import cse.buffalo.edu.algorithms.stdlib.StdIn;
+import cse.buffalo.edu.algorithms.stdlib.StdOut;
+
+public class Insertion {
+  public static void sort(Comparable[] a) {
+    for (int i = 0; i < a.length; i++) {
+      for (int j = i; j > 0 && less(a[j], a[j - 1]); j--) {
+        exch(a, j, j - 1);
+      }
+    }
+  }
+
+  private static boolean less(Comparable v, Comparable w) {
+    return (v.compareTo(w) < 0);
+  }
+
+  private static void exch(Comparable[] a, int i, int j) {
+    Comparable tmp = a[i];
+    a[i] = a[j];
+    a[j] = tmp;
+  }
+
+  private static void show(Comparable[] a) {
+    for (int i = 0; i < a.length; i++) {
+      StdOut.println(a[i]);
+    }
+  }
+
+  public static void main(String[] args) {
+    String[] a = StdIn.readStrings();
+    Insertion.sort(a);
+    show(a);
+  }
+}
