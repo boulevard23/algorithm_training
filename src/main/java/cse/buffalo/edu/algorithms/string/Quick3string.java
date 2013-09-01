39e95037edd88279a0e8b2a02d227d22e78da329
Chao Zhang <zhang.chao.eric@gmail.com>
2013-09-01 11:45:08 -0400

    Implemented MSD

--- src/main/java/cse/buffalo/edu/algorithms/string/Quick3string.java
+++ src/main/java/cse/buffalo/edu/algorithms/string/Quick3string.java
@@ -3,57 +3,37 @@ package cse.buffalo.edu.algorithms.string;
 import cse.buffalo.edu.algorithms.stdlib.StdIn;
 import cse.buffalo.edu.algorithms.stdlib.StdOut;
 
-public class MSD {
-
-  private static final int R = 256;  // extended ASCII alphabet size
+public class Quick3string {
   private static final int CUTOFF = 15;
 
   public static void sort(String[] a) {
-    int N = a.length;
-    String[] aux = new String[N];
-    sort(a, 0, N-1, 0, aux);
+    sort(a, 0, a.length-1, 0);
   }
 
-  // Return dth character of s, -1 if d = length of string
   private static int charAt(String s, int d) {
     if (d == s.length()) return -1;
     return s.charAt(d);
   }
 
-  // d means dth character
-  private static void sort(String[] a, int lo, int hi, int d, String[] aux) {
+  private static void sort(String[] a, int lo, int hi, int d) {
     if (hi <= lo + CUTOFF) {
       insertion(a, lo, hi, d);
       return;
     }
 
-    // This place is not R+1 but R+2, because there is one more end character -1
-    int[] count = new int[R+2];
-    for (int i = lo; i <= hi; i++) {
-      int c = charAt(a[i], d);
-      count[c+2]++;
+    int lt = lo, gt = hi;
+    int v = charAt(a[lo], d);
+    int i = lo + 1;
+    while (i <= gt) {
+      int t = charAt(a[i], d);
+      if (t < v) exch(a, lt++, i++);
+      else if (t > v) exch(a, i, gt--);
+      else i++;
     }
 
-    for (int r = 0; r < R+1; r++) {
-      count[r+1] += count[r];
-    }
-
-    // Move data
-    for (int i = lo; i <= hi; i++) {
-      int c = charAt(a[i], d);
-      // c+1 here is in case the end character -1, count[-1] is not correct
-      aux[count[c+1]++] = a[i];
-    }
-
-    // Copy back
-    for (int i = lo; i <= hi; i++) {
-      a[i] = aux[i - lo];
-    }
-
-    // Recursively sort for each character
-    for (int r = 0; r < R; r++) {
-      sort(a, lo + count[r], lo + count[r+1] - 1, d+1, aux);
-    }
+    sort(a, lo, lt-1, d);
+    if (v >= 0) sort(a, lt, gt, d+1);
+    sort(a, gt+1, hi, d);
   }
 
   private static void insertion(String[] a, int lo, int hi, int d) {
@@ -64,7 +44,7 @@ public class MSD {
     }
   }
 
-  private static void exch(String[] a, int i, int j) {
+    private static void exch(String[] a, int i, int j) {
     String temp = a[i];
     a[i] = a[j];
     a[j] = temp;
@@ -81,7 +61,9 @@ public class MSD {
   public static void main(String[] args) {
     String[] a = StdIn.readStrings();
     int N = a.length;
+
     sort(a);
+
     for (int i = 0; i < N; i++) {
       StdOut.println(a[i]);
     }
